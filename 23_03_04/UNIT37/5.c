86c68ade37e1475dfe082bac361d078bd318827e
HYUNSUNG <118896693+Hyuncastl@users.noreply.github.com>
2023-03-06 10:47:19 +0900

    Create 5.c

---
+++ 23_03_04/UNIT37/5.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <string .h>
+
+int main ()
+{
+  char *s1 = "PACHELBEL CANON";
+  char *s2 = "PACHELBEL CANON";
+  
+  int ret = strcmp(s1,s2);
+  
+  printf("%d,ret);
+  
+  return 0;
+}
